ba9409c8eed7730fcb02450a8c2b58e524ca601f
N. Schmidt <n0raitor@pm.me>
2025-10-06 16:13:40 +0200

    Remove Redundant Scripts

--- scripts/wsl-22.04-install-siftws.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update -y && sudo apt upgrade -y
-cd ~
-wget https://github.com/ekristen/cast/releases/download/v0.16.24/cast-v0.16.24-linux-amd64.deb
-sudo dpkg -i cast-v0.16.24-linux-amd64.deb
-sudo cast install --mode=server teamdfir/sift-saltstack
-sudo apt install plaso -y
--- scripts/wsl-install-vol3.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update -y && sudo apt upgrade -y
-cd ~
-git clone https://github.com/volatilityfoundation/volatility3.git
-cd volatility3/
-sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
-pip3.10 install -r requirements.txt
-echo "alias vol.py='python3 ~/volatility3/vol.py'" >> ~/.bashrc
-echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
